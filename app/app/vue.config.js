9a1ae0899b297eab5635fce02ba9d0ede919f713
DaChese <mealdo@umich.edu>
2025-04-22 20:27:30 +0000

    adjusted the vue.config.js file to point directly towards the forwarded address, if not there was an error.

--- app/app/vue.config.js
+++ app/app/vue.config.js
@@ -2,3 +2,11 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  devServer: {
+    client: {
+      webSocketURL: 'https://reimagined-space-barnacle-q7p5w75vvj4x24q9r-8080.app.github.dev/'
+   }
+  }
+}
