834af5fed8c9e8ce27c3b2a73a10ca59532a3242
Aldo Medina <mealdo@umich.edu>
2025-04-22 20:02:22 +0000

    testing the app, changed the title

--- app/app/src/App.vue
+++ app/app/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Testing the Vue.js App set up by Aldo Medina"/>
 </template>
 
 <script>
